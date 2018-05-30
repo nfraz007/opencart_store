b03a9de01ebeaa9c845dd775fab20ac6ba1b277f
nfraz007 <nfraz007@gmail.com>
2018-05-30 23:45:23 +0530

    first setup

---
+++ storage/cache/2b/2bb71163e193682cee96587f9b62766e4a06d75861824dce3e2778830c064d7b.php
@@ -0,0 +1,45 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_52b2c0da24a003a5f41430946f883604f85133c3a875260bd5ae6fc208fbc01d extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer id=\"footer\">";
+        echo (isset($context["text_footer"]) ? $context["text_footer"] : null);
+        echo "<br />";
+        echo (isset($context["text_version"]) ? $context["text_version"] : null);
+        echo "</footer></div>
+</body></html>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
+/* <footer id="footer">{{ text_footer }}<br />{{ text_version }}</footer></div>*/
+/* </body></html>*/
+/* */
---
+++ storage/cache/69/6923800fc09e87d5af563c95a2dbc7d1f3d29bb82a2ceab2f3b795c32a49da96.php
@@ -0,0 +1,52 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_4874d9c7f8754df1a6d6f07f9972e2ee6030e919095a8c80cbeeea720aabf6eb extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\"><a href=\"http://www.opencart.com\" target=\"_blank\">";
+        // line 2
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>|<a href=\"http://www.opencart.com/index.php?route=documentation/introduction\" target=\"_blank\">";
+        echo (isset($context["text_documentation"]) ? $context["text_documentation"] : null);
+        echo "</a>|<a href=\"http://forum.opencart.com\" target=\"_blank\">";
+        echo (isset($context["text_support"]) ? $context["text_support"] : null);
+        echo "</a><br />
+    </div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 2,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container"><a href="http://www.opencart.com" target="_blank">{{ text_project }}</a>|<a href="http://www.opencart.com/index.php?route=documentation/introduction" target="_blank">{{ text_documentation }}</a>|<a href="http://forum.opencart.com" target="_blank">{{ text_support }}</a><br />*/
+/*     </div>*/
+/* </footer>*/
+/* </body></html>*/
